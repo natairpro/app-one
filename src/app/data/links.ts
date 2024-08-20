08b9e8d2c51c18dbfb2fdc8646da75b46c92eb4b
Chris Nattress <cnattress@gmail.com>
2024-08-19 18:03:54 -0600

    Adding initial files.

---
+++ src/app/data/links.ts
@@ -0,0 +1,114 @@
+export function LngLinks() {
+    return [{
+        name: 'Evas Intimates',
+        url: 'https://www.evasintimates.com'
+      },{
+        name: 'Lions Den',
+        url: 'https://shop.lionsden.com'
+      },{
+        name: 'Spicy Lingerie',
+        url: 'https://www.spicylingerie.com'
+      },{
+        name: 'Yandy',
+        url: 'https://www.yandy.com'
+      },{
+        name: 'Fredricks of Hollywood',
+        url: 'https://www.fredericks.com'
+      },{
+        name: 'Fleur Du Mal',
+        url: 'https://fleurdumal.com'
+      },{
+        name: 'Miss Exotic',
+        url: 'https://missexotic.com'
+      },{
+        name: 'Love Good Lingerie',
+        url: 'https://lovegoodlingerie.com'
+      },{
+        name: 'Nasty Gal',
+        url: 'https://www.nastygal.com'
+      },{
+        name: 'Skarlett Blue',
+        url: 'https://www.skarlettblue.com'
+      },{
+        name: 'Hunkemoller',
+        url: 'https://www.hunkemoller.com'
+      },{
+        name: 'Haute Flair',
+        url: 'https://hauteflair.com'
+      },{
+        name: '3Wishes',
+        url: 'https://www.3wishes.com'
+      },{
+        name: 'Obsessive',
+        url: 'https://obsessive.com'
+      },{
+        name: 'Oleanda',
+        url: 'https://www.oleanda.com'
+      },{
+        name: 'Bedtime Flirt',
+        url: 'https://www.bedtimeflirt.com'
+      },{
+        name: 'Maison Close',
+        url: 'https://www.maison-close.us'
+      },{
+        name: 'Moxy Intimates',
+        url: 'https://moxyintimates.com'
+      },{
+        name: 'Bluebella',
+        url: 'https://www.bluebella.us/'
+      },{
+        name: 'Avec Amour Lingerie',
+        url: 'https://avecamourlingerie.com'
+      },{
+        name: 'Evas Boutique',
+        url: 'https://evasboutique.com'
+      },{
+        name: 'Honey Birdette',
+        url: 'https://us.honeybirdette.com'
+      },{
+        name: 'Darkest Fox',
+        url: 'https://darkestfox.com'
+      },{
+        name: 'Lingerie Living',
+        url: 'https://lingerieliving.com'
+      },{
+        name: 'Fancy Collect',
+        url: 'https://fancycollect.com'
+      },{
+        name: 'Body Kandi',
+        url: 'https://www.bodykandi.com'
+      },{
+        name: 'Sparkling Strawberry',
+        url: 'https://www.sparklingstrawberry.com'
+      },{
+        name: 'Dream Girl Official',
+        url: 'https://www.dreamgirlofficial.com'
+      },{
+        name: 'Macondo Trading',
+        url: 'https://macondotrading.com'
+      },{
+        name: 'Midnight Divas',
+        url: 'https://midnightdivas.com'
+      },{
+        name: 'Lingerie Mart',
+        url: 'https://www.lingeriemart.com'
+      },{
+        name: 'Urclover',
+        url: 'https://www.urclover.com/'
+      },{
+        name: 'Lavah Intimates',
+        url: 'https://lavahintimates.com/'
+      },{
+        name: 'Elin Harness',
+        url: 'https://elinharness.com/'
+      },{
+        name: 'Sister Party',
+        url: 'https://www.sisterparty.com'
+      },{
+        name: 'Avid Love',
+        url: 'https://avidlove.com'
+      },{
+        name: 'Lale Look',
+        url: 'https://www.lalelook.com/'
+      }];
+}
