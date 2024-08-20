68ea8de09f703510e738c03fe2ef0419e3162b52
Chris Nattress <cnattress@gmail.com>
2024-08-19 18:21:36 -0600

    Adding material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LngLinks } from './data/links';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
